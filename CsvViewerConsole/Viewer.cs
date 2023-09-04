979ef38ddd7e36495a919a372d5460393c0fade2
Dario <dariocantella@gmail.com>
2023-09-04 11:12:58 +0200

    Refactored Viewer Class

--- CsvViewerConsole/Viewer.cs
+++ CsvViewerConsole/Viewer.cs
@@ -1,118 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsvViewerConsole
 {
-    class Viewer 
+    internal class Viewer
     {
-        //declare filename and linlength
-        private string csvFilePath;
-        private int pageLength;
-        private string[] csvData;
-        private string firstLine;
+        private readonly string _csvFilePath;
+        private readonly int _pageLength;
+        private string[] _csvData;
+        private string _firstLine;
+
         public Viewer(string filename, int pagelength)
         {
-            csvFilePath = filename;
-            pageLength = pagelength;
+            _csvFilePath = filename;
+            _pageLength = pagelength;
         }
+
+        // Entry point
         public void Run()
         {
-            csvData = File.ReadAllLines(csvFilePath);
-            firstLine = csvData[0];
-            csvData = csvData.Skip(1).ToArray();
-            Show(csvData, pageLength);
-        } 
+            // Read file and separate first line from the rest. (local function)
+            (string first, string[] rest) ReadAndSeparateLines(string path)
+            {
+                var lines = File.ReadAllLines(path);
+                return (lines.First(), lines.Skip(1).ToArray());
+            }
 
+            // Read file and separate first line from the rest
+            (_firstLine, _csvData) = ReadAndSeparateLines(_csvFilePath);
+
+            // Display the CSV data and the menu
+            Show(_csvData, _pageLength);
+        }
+
+        // Display the CSV data and the menu 
         private void Show(string[] csvData, int pageLength)
         {
-            int currentPageStart = 0;
+            // Variables to keep track of the current page and total pages.
+            int currentPageIndex = 0;
+            int totalPages = (csvData.Length + pageLength - 1) / pageLength;  // Calculate total pages using ceiling division.
 
+            // Local function to get the current page of CSV data.
+            string[] GetCurrentPage() => csvData.Skip(currentPageIndex).Take(pageLength).ToArray();
+
+            // Dictionary to map user commands to corresponding actions.
+            Dictionary<char, Action> commands = new Dictionary<char, Action>
+            {
+                { 'e', () => Environment.Exit(0) },  // Exit the application
+                { 'f', () => currentPageIndex = 0 },  // Move to the first page
+                { 'l', () => currentPageIndex = (totalPages - 1) * pageLength },  // Move to the last page
+                { 'n', () => currentPageIndex = Math.Min((totalPages - 1) * pageLength, currentPageIndex + pageLength) },  // Move to the next page
+                { 'p', () => currentPageIndex = Math.Max(0, currentPageIndex - pageLength) }  // Move to the previous page
+            };
+
+            // Local functions for clarity
+            void DisplayCurrentPageAndOptions()
+            {
+                var currentPage = GetCurrentPage();
+                DisplayCsvPage(currentPage, _firstLine);
+                Console.Write("F)irst, L)ast, N)ext, P)rev, E)xit: ");
+            }
+
+            char GetUserCommand() => char.ToLower(Console.ReadKey().KeyChar);
+
+            void ExecuteCommand(char command)
+            {
+                Console.WriteLine("\n");
+                if (commands.ContainsKey(command))
+                {
+                    commands[command]();
+                }
+            }
+
+            // Main loop for pagination and user interaction.
             while (true)
             {
-                var currentPage = csvData.Skip(currentPageStart).Take(pageLength).ToList();
-
-                DisplayCsvPage(currentPage, firstLine);
-
-                Console.Write("F)irst, L)ast, N)ext, P)rev, E)xit: ");
-                var command = char.ToLower(Console.ReadKey().KeyChar);
-                Console.WriteLine("\n");
-
-                switch (command)
-                {
-                    case 'e': // exit
-                        return;
-                    case 'f': // first page
-                        currentPageStart = 0;
-                        break;
-                    case 'l': // last page
-                        currentPageStart = Math.Max(0, csvData.Length - pageLength);
-                        break;
-                    case 'n': // next page
-                        currentPageStart += pageLength;
-                        currentPageStart = Math.Min(csvData.Length - pageLength, currentPageStart);
-                        break;
-                    case 'p': // previous page
-                        currentPageStart -= pageLength;
-                        currentPageStart = Math.Max(0, currentPageStart);
-                        break;
-                }
+                DisplayCurrentPageAndOptions();
+                var command = GetUserCommand();
+                ExecuteCommand(command);
             }
         }
 
-        private static void DisplayCsvPage(List<string> pageData, string firstLine)
+        // Method to display a single page of CSV data.
+        private static void DisplayCsvPage(IReadOnlyList<string> pageData, string firstLine)
         {
-            Console.Clear();
+            // Local function to generate the separator line based on column widths.
+            string GenerateSeparator(List<int> widths) =>
+                "+" + string.Join("+", widths.Select(w => new string('-', w + 1))) + "+";
+
+            // Calculate the column widths based on the first line and data.
             var columnWidths = GetColumnWidths(pageData, firstLine);
 
-            var separatorFields = columnWidths.Select(width => new string('-', width+1));
-            var separatorLine = "+" + string.Join("+", separatorFields) + "+";
+            // Generate the separator line.
+            var separator = GenerateSeparator(columnWidths);
 
-            Console.WriteLine(separatorLine);
-            Console.WriteLine(CreateDisplayLine(firstLine, columnWidths));
-            Console.WriteLine(separatorLine);
+            // Clear the console for fresh output.
+            Console.Clear();
 
-           
-            for (int i = 0; i < pageData.Count; i++)
-            {
-                Console.WriteLine(CreateDisplayLine(pageData[i], columnWidths));
-            }
+            // Action to print a line in a formatted manner.
+            Action<string> printLine = line => Console.WriteLine(CreateDisplayLine(line, columnWidths));
 
-            Console.WriteLine(separatorLine);
+            // Display the header and separator.
+            new[] { separator, firstLine }.ToList().ForEach(printLine);
+            Console.WriteLine(separator);
+
+            // Display each data line.
+            pageData.ToList().ForEach(printLine);
+
+            // Display the closing separator.
+            Console.WriteLine(separator);
         }
 
-        private static List<int> GetColumnWidths(List<string> pageData, string firstLine)
+        // Method to calculate the maximum width for each column.
+        private static List<int> GetColumnWidths(IReadOnlyList<string> pageData, string firstLine)
         {
+            // Local function to split and trim each field in a line.
             var headerFields = firstLine.Split(';').Select(field => field.Trim()).ToList();
-            var columnWidths = new List<int>();
 
-            for (int i = 0; i < headerFields.Count; i++)
-            {
-                int maxWidth = headerFields[i].Length;
-                for (int j = 1; j < pageData.Count; j++)
-                {
-                    var rowData = pageData[j].Split(';').Select(field => field.Trim()).ToList();
-                    maxWidth = Math.Max(maxWidth, rowData[i].Length);
-                }
-                columnWidths.Add(maxWidth);
-            }
-
-            return columnWidths;
+            // Calculate the maximum width for each column.
+            return headerFields.Select((header, index) =>
+                    pageData.Select(row => row.Split(';').ElementAtOrDefault(index)?.Trim().Length ?? 0)
+                        .Append(header.Length)
+                        .Max())
+                        .ToList();
         }
 
-        private static string CreateDisplayLine(string dataLine, List<int> columnWidths)
+        // Method to format a line for display.
+        private static string CreateDisplayLine(string dataLine, IReadOnlyList<int> columnWidths)
         {
-            var fields = dataLine.Split(';').Select(field => field.Trim()).ToList();
-            var displayFields = new List<string>();
+            // Split and trim each field in the line.
+            var fields = dataLine.Split(';').Select(field => field.Trim());
 
-            for (int i = 0; i < fields.Count; i++)
-            {
-                displayFields.Add(fields[i].PadRight(columnWidths[i]+1));
-            }
+            // Pad each field to match the corresponding column width.
+            var paddedFields = fields.Zip(columnWidths, (field, width) => field.PadRight(width + 1));
 
-            return "|" + string.Join("|", displayFields) + "|";
-        }       
+            // Join the padded fields with pipe characters to create the display line.
+            return "|" + string.Join("|", paddedFields) + "|";
+        }
     }
 }
